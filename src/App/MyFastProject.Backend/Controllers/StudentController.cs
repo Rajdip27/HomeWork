31ab2a7b43ea49b0fed70506dbcc64ce3dbb7ed7
Rajdip27 <Srajdip920@gmail.com>
2023-07-17 18:43:53 +0600

    AddGetById Method

--- src/App/MyFastProject.Backend/Controllers/StudentController.cs
+++ src/App/MyFastProject.Backend/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using static MyFastProject.Core.Student.Command.CreateStudent;
+using static MyFastProject.Core.Student.Query.GetStudentByIdQuery;
 
 namespace MyFastProject.Backend.Controllers;
 
@@ -17,6 +18,12 @@ public class StudentController : ControllerBase
 	public async Task<ActionResult<Service.Models.ViewModel.Student>> Create([FromBody] Service.Models.ViewModel.Student qurey)
 	{
 		var result = await _mediator.Send(new CreateStudentCommand(qurey));
+		return Ok(result);
+	}
+	[HttpGet("{id}")]
+	public async Task<ActionResult<Service.Models.ViewModel.Student>> GetStudentById(int id)
+	{
+		var result= await _mediator.Send(new GetStudentQueryById(id));
 		return Ok(result);
 	}
 }
---
+++ src/libraries/infrastructure/MyFastProject.Core/Student/Query/GetStudentByIdQuery.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using MyFastProject.Repositories.Interfaces;
+using System.Text.Json.Serialization;
+
+namespace MyFastProject.Core.Student.Query;
+
+public class GetStudentByIdQuery
+{
+	public class GetStudentQueryById : IRequest<Service.Models.ViewModel.Student> 
+	{
+		[JsonConstructor]
+		public GetStudentQueryById(int id) { 
+			Id = id;
+		}
+		public int Id { get; set; }
+	}
+	public class GetStudentQueryByIdHandler : IRequestHandler<GetStudentQueryById, Service.Models.ViewModel.Student>
+	{
+		private readonly IStudentRepository _studentRepository;
+		
+        public GetStudentQueryByIdHandler(IStudentRepository studentRepository)
+        {
+			_studentRepository = studentRepository;
+				
+        }
+        public async Task<Service.Models.ViewModel.Student> Handle(GetStudentQueryById request, CancellationToken cancellationToken)
+		{
+			return await _studentRepository.GetById(request.Id);
+		}
+	}
+}
